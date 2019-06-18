79d737ef98ad197cfec42d506be3a1f63f2cdfcb
Bruno Medina <bmedinab@gmail.com>
2019-06-17 21:52:04 -0700

    Ruby app had a css missing

--- sample-applications/redis-ruby-client/Blog/app/helpers/articles_helper.rb
+++ sample-applications/redis-ruby-client/Blog/app/helpers/articles_helper.rb
@@ -2,9 +2,12 @@ module ArticlesHelper
     def fetch_articles_redis
         articles = $redis.get("articles")  #This line requests redis-server to accepts any value associate with articles key
         if articles.nil?  #this condition will executes if any articles not available on redis server
-            articles = Article.first(100).to_json   
+            articles = Article.first(100).to_json
+            print("Articles obtained from database")   
             $redis.set("articles",articles)
             $redis.expire("articles",5.minutes.to_i)
+        else
+            print("Articles obtained from redis")   
         end
         JSON.load articles #This will converts JSON data to Ruby Hash
     end
